c41e4158adce4a768081eeb407f545b00e78a984
Víctor M. Bartolomé <victorcuevas1790@gmail.com>
2025-05-20 17:06:42 +0200

    Se pueden crear cuidades y puntos de interes

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">GRUPO D</a>'
+}).addTo(map);
+
+
+
 const arrayCiudades = []
 
-function crearCiudad(nombre, latitud, longitud) {
+function crearCiudad(nombre, latitud, longitud,puntosInteres) {
     return {
       nombre,
       latitud,
       longitud,
       puntosInteres
     };
-  }
+}
 
-  const roma = crearCiudad(Roma,-29.44781,27.71233)
+function crearPuntosInteres(nombre, latitud, longitud, categoria){
+    return{
+        nombre,
+        latitud,
+        longitud,
+        categoria
+    }
+}
 
-  arrayCiudades.push(roma);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">GRUPO D</a>'
-}).addTo(map);
+//DATOS DE PUNTOS DE INTERÉS
+let coliseo = crearPuntosInteres("Coliseo", 41.890274, 12.492238, "Historico");
+let sanPedro = crearPuntosInteres("Basílica de San Pedro", 41.902321, 12.453625, "Religioso");
+
+
+
+//CREACCIÓN DE CIUDADES
+const roma = crearCiudad(Roma,-29.44781,27.71233,[coliseo, sanPedro, ]);
