a8dd0c7d746a4eb3a752b7b1972bf8a14b9b9951
Víctor M. Bartolomé <victorcuevas1790@gmail.com>
2025-05-22 13:34:59 +0200

    Muestra direcciones en select
    Si existe el select, lo borra y muestra las direcciones.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 const arrayDireccionesEncontradas = [];
 
+const arrayCiudades = [];
+
 const selectCiudad = document.getElementById("ciudad");
 
-let mapa, ciudad ="";
+let mapa, ciudad = "";
 let marcadorActual = null;
 let poligono = null;
 
@@ -54,18 +56,18 @@ const nuevaYork = crearCiudad("Nueva York", 40.71285, -74.00632, [centralPark, e
 const poligonoRoma = [
     [
         [
-            [41.9756, 12.4176],[41.9904, 12.5000],[41.9774, 12.5488],[41.96038, 12.58158],[41.92335, 12.61797],[41.90215, 12.62381]
-            ,[41.87122, 12.60836],[41.85051, 12.59926],[41.81303, 12.57557],[41.79512, 12.53746],[41.79486, 12.48184],[41.80664, 12.41060]
-            ,[41.81572, 12.38863],[41.84425, 12.37816],[41.86406, 12.37061],[41.91339, 12.38228],[41.95477, 12.38005],[41.96983, 12.40030]
-            
+            [41.9756, 12.4176], [41.9904, 12.5000], [41.9774, 12.5488], [41.96038, 12.58158], [41.92335, 12.61797], [41.90215, 12.62381]
+            , [41.87122, 12.60836], [41.85051, 12.59926], [41.81303, 12.57557], [41.79512, 12.53746], [41.79486, 12.48184], [41.80664, 12.41060]
+            , [41.81572, 12.38863], [41.84425, 12.37816], [41.86406, 12.37061], [41.91339, 12.38228], [41.95477, 12.38005], [41.96983, 12.40030]
+
         ]
     ],
     [
         [
-            [41.90739, 12.45536],[41.90625, 12.45588],[41.90590, 12.45781],[41.90327, 12.45777],[41.90253, 12.45841],[41.90124, 12.45824]
-            ,[41.90132, 12.45624],[41.90122, 12.45474],[41.90017, 12.45468],[41.90009, 12.45294],[41.90047, 12.45099],[41.90062, 12.44790]
-            ,[41.90188, 12.44562],[41.90244, 12.44657],[41.90309, 12.44772],[41.90446, 12.44886],[41.90630, 12.45058],[41.90657, 12.45152]
-            ,[41.90707, 12.45393]
+            [41.90739, 12.45536], [41.90625, 12.45588], [41.90590, 12.45781], [41.90327, 12.45777], [41.90253, 12.45841], [41.90124, 12.45824]
+            , [41.90132, 12.45624], [41.90122, 12.45474], [41.90017, 12.45468], [41.90009, 12.45294], [41.90047, 12.45099], [41.90062, 12.44790]
+            , [41.90188, 12.44562], [41.90244, 12.44657], [41.90309, 12.44772], [41.90446, 12.44886], [41.90630, 12.45058], [41.90657, 12.45152]
+            , [41.90707, 12.45393]
         ]
     ]
 ];
@@ -73,44 +75,50 @@ const poligonoRoma = [
 
 
 const poligonoZamora = [
-    [41.49190, -5.75954], [41.49517, -5.76096], [41.49739, -5.76066],[41.50099, -5.76229],[41.50340, -5.76370],[41.51141, -5.77314],[41.51854, -5.76872],[41.52737, -5.76752],[41.52991, -5.75649],[41.52294, -5.74782],[41.52281, -5.73280],[41.52435, -5.71993],[41.52304, -5.71263],[41.51713, -5.71388],[41.49849, -5.72752],
-    [41.48630, -5.73877],[41.48206, -5.75379],[41.48617, -5.75980]
+    [41.49190, -5.75954], [41.49517, -5.76096], [41.49739, -5.76066], [41.50099, -5.76229], [41.50340, -5.76370]
+    ,[41.51141, -5.77314], [41.51854, -5.76872], [41.52737, -5.76752], [41.52991, -5.75649], [41.52294, -5.74782]
+    ,[41.52281, -5.73280], [41.52435, -5.71993], [41.52304, -5.71263], [41.51713, -5.71388], [41.49849, -5.72752],
+    [41.48630, -5.73877], [41.48206, -5.75379], [41.48617, -5.75980]
 ]
 
 const poligonoLugo = [
-    [43.03213, -7.57250],[43.03514, -7.55499],[43.03427, -7.54383],[43.03289, -7.53405],[43.02322, -7.52460]
-    ,[43.02084, -7.52443],[43.00364, -7.52941],[42.99259, -7.53336],[42.98770, -7.54211],[42.99536, -7.56065]
-    ,[43.00050, -7.56065],[43.00126, -7.56598],[43.00515, -7.57696],[43.01557, -7.58434],[43.02511, -7.58074]
+    [43.03213, -7.57250], [43.03514, -7.55499], [43.03427, -7.54383], [43.03289, -7.53405], [43.02322, -7.52460]
+    , [43.02084, -7.52443], [43.00364, -7.52941], [42.99259, -7.53336], [42.98770, -7.54211], [42.99536, -7.56065]
+    , [43.00050, -7.56065], [43.00126, -7.56598], [43.00515, -7.57696], [43.01557, -7.58434], [43.02511, -7.58074]
 ]
 
-const poligonoParis= [
-    [48.85342, 2.22422],[48.86596, 2.23074],[48.87296, 2.24293],[48.88030, 2.26027],[48.88346, 2.28344]
-    ,[48.89463, 2.30404],[48.90005, 2.32018],[48.90084, 2.35811],[48.90163, 2.39004],[48.88222, 2.40274]
-    ,[48.87262, 2.41425],[48.86539, 2.41837],[48.85173, 2.41665],[48.83489, 2.41236],[48.82438, 2.38798]
-    ,[48.81432, 2.36361],[48.81512, 2.33597],[48.82540, 2.29357],[48.83422, 2.26233],[48.82574, 2.25151]
-    ,[48.82676, 2.22782],[48.83229, 2.22610],[48.83806, 2.22267]
+const poligonoParis = [
+    [48.85342, 2.22422], [48.86596, 2.23074], [48.87296, 2.24293], [48.88030, 2.26027], [48.88346, 2.28344]
+    , [48.89463, 2.30404], [48.90005, 2.32018], [48.90084, 2.35811], [48.90163, 2.39004], [48.88222, 2.40274]
+    , [48.87262, 2.41425], [48.86539, 2.41837], [48.85173, 2.41665], [48.83489, 2.41236], [48.82438, 2.38798]
+    , [48.81432, 2.36361], [48.81512, 2.33597], [48.82540, 2.29357], [48.83422, 2.26233], [48.82574, 2.25151]
+    , [48.82676, 2.22782], [48.83229, 2.22610], [48.83806, 2.22267]
 ]
 
 const poligonoNuevaYork = [
     [
         [
-            [40.91727, -73.91812],[40.87120, -73.74985],[40.84575, -73.75792],[40.82614, -73.77989],[40.80483, -73.78178],[40.80510, -73.81491]
-            ,[40.79769, -73.85971],[40.79353, -73.90606],[40.78287, -73.92031],[40.77806, -73.93370],[40.73996, -73.96254],[40.73658, -73.96357]
-            ,[40.72097, -73.96425],[40.68570, -74.00923],[40.68011, -74.02193],[40.68428, -74.03532],[40.70081, -74.02657],[40.75649, -74.01403]
-            ,[40.82706, -73.96357],[40.91364, -73.91962]
+            [40.91727, -73.91812], [40.87120, -73.74985], [40.84575, -73.75792], [40.82614, -73.77989], [40.80483, -73.78178], [40.80510, -73.81491]
+            , [40.79769, -73.85971], [40.79353, -73.90606], [40.78287, -73.92031], [40.77806, -73.93370], [40.73996, -73.96254], [40.73658, -73.96357]
+            , [40.72097, -73.96425], [40.68570, -74.00923], [40.68011, -74.02193], [40.68428, -74.03532], [40.70081, -74.02657], [40.75649, -74.01403]
+            , [40.82706, -73.96357], [40.91364, -73.91962]
         ]
     ],
     [
         [
-            [40.65134, -74.05583],[40.62691, -74.05721],[40.57628, -74.03489],[40.50818, -74.04390],[40.4775, -74.2264],[40.48684, -74.25299]
-            ,[40.50838, -74.25822],[40.52110, -74.24621],[40.54622, -74.24878],[40.55933, -74.23127],[40.55770, -74.21702],[40.58749, -74.20741]
-            ,[40.60118, -74.19994],[40.61441, -74.20381],[40.62275, -74.20312],[40.63160, -74.20226],[40.63662, -74.19694],[40.64554, -74.18570]
-            ,[40.64671, -74.18166],[40.64163, -74.14149],[40.64339, -74.13257],[40.64795, -74.10905],[40.64860, -74.09205],[40.65082, -74.08639]
-            ,[40.65121, -74.05952]
+            [40.65134, -74.05583], [40.62691, -74.05721], [40.57628, -74.03489], [40.50818, -74.04390], [40.4775, -74.2264], [40.48684, -74.25299]
+            , [40.50838, -74.25822], [40.52110, -74.24621], [40.54622, -74.24878], [40.55933, -74.23127], [40.55770, -74.21702], [40.58749, -74.20741]
+            , [40.60118, -74.19994], [40.61441, -74.20381], [40.62275, -74.20312], [40.63160, -74.20226], [40.63662, -74.19694], [40.64554, -74.18570]
+            , [40.64671, -74.18166], [40.64163, -74.14149], [40.64339, -74.13257], [40.64795, -74.10905], [40.64860, -74.09205], [40.65082, -74.08639]
+            , [40.65121, -74.05952]
         ]
     ]
 ];
 
+
+function reiniciarArrayCiudades(){
+    arrayCiudades.push(roma, zamora, lugo, paris, nuevaYork);
+}
 
 mapa = L.map('map').setView([0, 0], 0);
 
@@ -136,7 +144,7 @@ selectCiudad.addEventListener('change', () => {
 
 
 function seleccionarCiudad(ciudad) {
- 
+
     switch (ciudad) {
         case "roma":
             //agregar que se muestre la opción puntos de interés
@@ -146,7 +154,7 @@ function seleccionarCiudad(ciudad) {
             eliminarMarcador();
             agregarPoligono(poligonoRoma, 'green');
             mostrarPuntosInteres(ciudad);
-            
+
             break;
         case "zamora":
             mapa.setView([zamora.latitud, zamora.longitud], 13);
@@ -158,7 +166,7 @@ function seleccionarCiudad(ciudad) {
             break;
         case "lugo":
             mapa.setView([lugo.latitud, lugo.longitud], 13);
-           // agregarMarcador(lugo.latitud, lugo.longitud);
+            // agregarMarcador(lugo.latitud, lugo.longitud);
             eliminarPoligono();
             eliminarMarcador();
             agregarPoligono(poligonoLugo, 'orange');
@@ -166,7 +174,7 @@ function seleccionarCiudad(ciudad) {
             break;
         case "paris":
             mapa.setView([paris.latitud, paris.longitud], 10);
-           // agregarMarcador(paris.latitud, paris.longitud);
+            // agregarMarcador(paris.latitud, paris.longitud);
             eliminarPoligono();
             eliminarMarcador();
             agregarPoligono(poligonoParis, 'blue');
@@ -174,7 +182,7 @@ function seleccionarCiudad(ciudad) {
             break;
         case "nueva york":
             mapa.setView([nuevaYork.latitud, nuevaYork.longitud], 10);
-           // agregarMarcador(nuevaYork.latitud, nuevaYork.longitud);
+            // agregarMarcador(nuevaYork.latitud, nuevaYork.longitud);
             eliminarPoligono();
             eliminarMarcador();
             agregarPoligono(poligonoNuevaYork, 'purple');
@@ -191,67 +199,68 @@ function seleccionarCiudad(ciudad) {
     }
 }
 //Elimina el poligono anterior
-function eliminarPoligono(){
-    if(poligono != null){
+function eliminarPoligono() {
+    if (poligono != null) {
         mapa.removeLayer(poligono);
     }
 }
 //Agrega los poligonos
-function agregarPoligono(poligonoCiudad, colorPoligono){
-    poligono = L.polygon(poligonoCiudad, {color: colorPoligono}).addTo(mapa);
+function agregarPoligono(poligonoCiudad, colorPoligono) {
+    poligono = L.polygon(poligonoCiudad, { color: colorPoligono }).addTo(mapa);
     mapa.fitBounds(poligono.getBounds());
 }
 
-function mostrarPuntosInteres(ciudad){
-   
-    if(ciudad === 'roma'){
-        for(let i = 0 ; i < 5; i++){
+function mostrarPuntosInteres(ciudad) {
+
+    if (ciudad === 'roma') {
+        for (let i = 0; i < 5; i++) {
             let punto = roma.puntosInteres[i];
             agregarMarcador(punto.latitudPunto, punto.longitudPunto);
         }
     }
-    if(ciudad === 'zamora'){
-        for(let i = 0 ; i < 5; i++){
+    if (ciudad === 'zamora') {
+        for (let i = 0; i < 5; i++) {
             let punto = zamora.puntosInteres[i];
             agregarMarcador(punto.latitudPunto, punto.longitudPunto);
         }
     }
-    if(ciudad === 'lugo'){
-        for(let i = 0 ; i < 5; i++){
+    if (ciudad === 'lugo') {
+        for (let i = 0; i < 5; i++) {
             let punto = lugo.puntosInteres[i];
             agregarMarcador(punto.latitudPunto, punto.longitudPunto);
         }
     }
-    if(ciudad === 'paris'){
-        for(let i = 0 ; i < 5; i++){
+    if (ciudad === 'paris') {
+        for (let i = 0; i < 5; i++) {
             let punto = paris.puntosInteres[i];
             agregarMarcador(punto.latitudPunto, punto.longitudPunto);
         }
     }
-    if(ciudad === 'nueva york'){
-        for(let i = 0 ; i < 5; i++){
+    if (ciudad === 'nueva york') {
+        for (let i = 0; i < 5; i++) {
             let punto = nuevaYork.puntosInteres[i];
             agregarMarcador(punto.latitudPunto, punto.longitudPunto);
         }
     }
 
-        
-    
+
+
 
 
 
 }
 
 
-function eliminarMarcador(){
+function eliminarMarcador() {
     if (marcadorActual) {
         mapa.removeLayer(marcadorActual);
-    }}
+    }
+}
 /**
  * Función que agrega un marcador al mapa, elimiando el anterior
  * */
 function agregarMarcador(latitud, longitud) {
-    
+
     // Crear y agregar el nuevo marcador
     marcadorActual = L.marker([latitud, longitud]).addTo(mapa);
 }
@@ -295,22 +304,22 @@ function crearPuntosInteres(nombre, latitudPunto, longitudPunto, categoria) {
 /**
  * boton para añadir direcciones personalizadas
  */
-const boton1= document.getElementById("boton1")
+const boton1 = document.getElementById("boton1")
 boton1.addEventListener("click", () => {
     mostrarForm();
 });
 /**
  * funcion para mostrar el formulario paara añadir personalizacion
  */
-function mostrarForm(){
-    document.getElementById("boton1").style.display= "none";
+function mostrarForm() {
+    document.getElementById("boton1").style.display = "none";
     document.getElementById("añadir").style.display = "block";
 
 }
 /**
  * boton para guardar el input 
  */
-const boton2= document.getElementById("buscar")
+const boton2 = document.getElementById("buscar")
 boton2.addEventListener("click", () => {
     geocodificar();
     //mostrarSugerencias();
@@ -319,14 +328,14 @@ boton2.addEventListener("click", () => {
 
 
 function geocodificar() {
-      const direccion = document.getElementById('dirPersonalizada').value;
-      const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(direccion)}`;
+    const direccion = document.getElementById('dirPersonalizada').value;
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(direccion)}`;
 
-      fetch(url, {
+    fetch(url, {
         headers: {
-          'User-Agent': 'Maps/1.0 (fvereszeltia@gmail.com)' // Requerido por Nominatim
+            'User-Agent': 'Maps/1.0 (fvereszeltia@gmail.com)' // Requerido por Nominatim
         }
-      })
+    })
         .then(response => response.json())
         .then(data => {
             console.log(data)
@@ -336,32 +345,63 @@ function geocodificar() {
 
         })
         .catch(error => {
-          console.error('Error al geocodificar:', error);
-          document.getElementById('resultado').textContent = 'Error al buscar dirección.';
+            console.error('Error al geocodificar:', error);
+            document.getElementById('resultado').textContent = 'Error al buscar dirección.';
         });
 
-        console.log("Hola")
-    }
-  
-    /**
-     * mostrar array en sugernecias de busqueda
-     */
-    const sugerencias= document.getElementById("sugerencias");
+    console.log("Hola")
+}
 
-    function mostrarSugerencias(){
-        console.log("a")
-        const nuevoElemento = null;
-        console.log(arrayDireccionesEncontradas.length);
-            
-        for( let i = 0; i<arrayDireccionesEncontradas.length;i++){
-            
-            const opcion = document.createElement('option');
-            opcion.setAttribute("value", i);
-            let nod = document.createTextNode(arrayDireccionesEncontradas[i].display_name);  
-            opcion.appendChild(nod);
-           
+/**
+ * mostrar array en sugernecias de busqueda
+ */
+const divDirecciones = document.getElementById("muestraDirecciones");
+const sugerencias = document.getElementById("sugerencias");
+const dirAgregada = null;
 
-            document.getElementById('selector').appendChild(opcion);
-        }
-        
+function mostrarSugerencias() {
+    console.log("a")
+
+    const nuevoElemento = null;
+    console.log(arrayDireccionesEncontradas.length);
+
+    //Reinicia el div cada vez que se realiza una busqueda.
+    divDirecciones.innerHTML ="";
+    const selectExistente = document.getElementById("select");
+    
+
+    const select = document.createElement("select");
+    select.id = "selector";
+    divDirecciones.appendChild(select);
+
+    //Si hay más de una opción a mostrar(direcciones encontradas), crea la primera option con el mensaje informativo
+    if (arrayDireccionesEncontradas.length > 1) {
+        const opcionDefecto = document.createElement('option');
+        opcionDefecto.textContent = "Varias coincidencias encontradas, elija una."
+        opcionDefecto.disabled = true;
+        opcionDefecto.selected = true;
+        select.appendChild(opcionDefecto)
     }
+
+    //Crea tantas opciones en el select como direcciones encontradas    
+    for (let i = 0; i < arrayDireccionesEncontradas.length; i++) {
+
+        const opcion = document.createElement('option');
+        opcion.setAttribute("value", i);
+        let nod = document.createTextNode(arrayDireccionesEncontradas[i].display_name);
+        opcion.appendChild(nod);
+
+
+        document.getElementById('selector').appendChild(opcion);
+    }
+    //Recoge el valor de la opción elegida en el desplegable de direcciones.
+    select.addEventListener("change", function (event) {
+        const posDirElegida = event.target.value;
+        const textoSeleccionado = event.target.options[event.target.selectedIndex].text;
+
+        console.log("Valor seleccionado:", posDirElegida);
+        console.log("Texto seleccionado:", textoSeleccionado);
+    });
+}
+
+
